ea8da6245d7115cf404171d531c8df570a117c5e
shadow <marci.alex1@gmail.com>
2023-08-23 23:20:12 +0200

    huge penis

--- src/utils.ts
+++ src/utils.ts
@@ -44,7 +44,7 @@ export async function getCommits(
   return commits
     .filter((commit) => !!commit.commit.message)
     .map((commit) => ({
-      message: commit.commit.message.replace('WXF\-\d\-',''),
+      message: commit.commit.message.replace(/WXF\-\d\-/,''),
       hash: commit.sha,
     }));
 }
